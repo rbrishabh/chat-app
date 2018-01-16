4bfb2688ae53881a9340ee7780b12bde3f08efa6
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2018-01-16 14:33:49 +0530

    Added Connection and disconnection routes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const publicPath = path.join(__dirname, '../public');
-
+const socketIO = require('socket.io')
 const PORT = process.env.PORT || 3000;
 
 var app = express();
-
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(PORT, ()=>{
+io.on('connection', (socket)=>{
+    console.log('New connection made');
+
+    socket.on('disconnect', ()=>{
+        console.log('Connection from client closed');
+    });
+});
+
+server.listen(PORT, ()=>{
     console.log(`Server up on ${PORT}`);
 });
